6a767ba4b567b622df3a30f85adee03c1c2203d3
Jonathan Alexis Bello Lopez <jonathan.bellofg@gmail.com>
2024-09-25 21:06:17 -0600

    add: migration to astro 5.0 beta and font for titles

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -4,7 +4,7 @@ export default {
   theme: {
     extend: {
       fontFamily: {
-        title: '"Hendry", serif',
+        title: '"hendryregular", serif',
         sans: '"Montserrat", sans-serif',
       },
     },
